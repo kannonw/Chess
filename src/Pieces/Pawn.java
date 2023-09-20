0d1e82a2e556acdee80c993396f19371add079f8
kannon <kannonmarih@protonmail.com>
2023-09-20 14:25:00 -0300

    implemented pawn movements

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -27,7 +27,7 @@ public class Pawn extends Piece{
         if (0 < new_row && new_row < 7 && pos.Col < 7 && board[new_row][pos.Col + 1] != null && board[new_row][pos.Col + 1].getColor() != pawn.getColor())
             validMoves[new_row][pos.Col + 1] = true;
 
-        if (0 < new_row && new_row < 7 && 0 < pos.Col && board[new_row][pos.Col - 1] != null && board[new_row][pos.Col + 1].getColor() != pawn.getColor())
+        if (0 < new_row && new_row < 7 && 0 < pos.Col && board[new_row][pos.Col - 1] != null && board[new_row][pos.Col - 1].getColor() != pawn.getColor())
             validMoves[new_row][pos.Col - 1] = true;
 
         if (0 < new_row + direction && new_row + direction < 7 && board[new_row][pos.Col] == null && board[new_row + direction][pos.Col] == null && FirstMove)
