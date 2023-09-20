0cf697933ca76fa36f2581e4f6bd05438e6698af
kannon <kannonmarih@protonmail.com>
2023-09-19 22:28:37 -0300

    implemented pawn movement

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -1,6 +1,8 @@
 package Pieces;
+import Runtime.*;
 
 public class Pawn extends Piece{
+
 
     public Pawn(PieceColor color) {
         super(color);
@@ -8,8 +10,33 @@ public class Pawn extends Piece{
 
     @Override
     public boolean[][] PossibleMoves(Piece[][] board, Position pos) {
-        return new boolean[0][];
+        boolean[][] validMoves = new boolean[Screen.HEIGHT][Screen.WIDTH];
+        Piece pawn = board[pos.Row][pos.Col];
+        int direction, new_row;
+
+        if (pawn.getColor() == PieceColor.WHITE)
+            direction = -1;
+        else
+            direction = 1;
+
+        new_row = pos.Row + direction;
+
+        if (0 < new_row && new_row < 7 && board[new_row][pos.Col] == null)
+            validMoves[new_row][pos.Col] = true;
+
+        if (0 < new_row && new_row < 7 && pos.Col < 7 && board[new_row][pos.Col + 1] != null && board[new_row][pos.Col + 1].getColor() != pawn.getColor())
+            validMoves[new_row][pos.Col + 1] = true;
+
+        if (0 < new_row && new_row < 7 && 0 < pos.Col && board[new_row][pos.Col - 1] != null && board[new_row][pos.Col + 1].getColor() != pawn.getColor())
+            validMoves[new_row][pos.Col - 1] = true;
+
+        if (0 < new_row + direction && new_row + direction < 7 && board[new_row][pos.Col] == null && board[new_row + direction][pos.Col] == null && FirstMove)
+            validMoves[new_row + direction][pos.Col] = true;
+
+        return validMoves;
     }
+
+
 
     @Override
     public char getLetter() {
